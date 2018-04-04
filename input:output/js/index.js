8c146cbb71832b424b2c06b5f56759853a9f9fe7
parks238 <parks238@github.com>
2018-04-04 09:53:50 -0400

    lab

--- input:output/js/index.js
+++ input:output/js/index.js
@@ -1,21 +1,37 @@
 document.getElementById("submit").addEventListener("click",function(){
-  var your_name = document.getElementById("your_name").value;
-  var greeting = document.getElementById("greeting22")
-  if(your_name == "red"){
+  var fav_color = document.getElementById("fav_color").value;
+  var fav_number = document.getElementById("fav_number").value;
+  var greeting = document.getElementById("greeting22");
+  if(fav_color == "red"){
     
     greeting.style.background = "red";
-    } else if(your_name == "blue"){
+    } else if(fav_color == "blue"){
       
       greeting.style.background = "blue";
-    } else if(your_name == "green"){
+    } else if(fav_color == "green"){
       
       greeting.style.background = "green";
-    } else if(your_name == "yellow"){
+    } else if(fav_color == "yellow"){
       
       greeting.style.background = "yellow";
     }
   else {
      
      greeting.style.background = "black";
+  }
+  if(fav_number=="1"){
+   greeting.innerHTML="My favorite number is 1 too!"
+  }
+  else if (fav_number=="2"){
+    greeting.innerHTML="I don't like 2"
+  }
+  else if (fav_number=="4"){
+    greeting.innerHTML="I don't like 4"
+  }
+    else if (fav_number=="7"){
+    greeting.innerHTML="My favorite number is 7 too!"
+   }
+  else{
+    greeting.innerHTML="Cool!"
   }
 });
